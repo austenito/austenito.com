d25813fb63467fc9662dbf6b989b298282be5be0
Austen Ito <austen@austenito.com>
2022-07-16 14:40:04 -0400

    Update packages and deployment
    * Update dependencies
    * Dockerize javascript asset compilation
    * Dockerize serving of blog
    * Add GitHub actions workflow

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
-import Vue from 'vue';
+// import { Vue } from 'vue';
+// import WineRegion from './components/WineRegion';
+//
+// Vue.component('wine-region', WineRegion);
+//
+// new Vue({}).$mount('#app');
+
+import { createApp } from 'vue'
 import WineRegion from './components/WineRegion';
 
-Vue.component('wine-region', WineRegion);
-
-new Vue({}).$mount('#app');
+const app = createApp(WineRegion)
+app.mount('#app')
